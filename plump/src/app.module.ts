30f6438012fb60587c2816e96615188b78fced50
FelipeRibadeneira <fribadenei@constructor.university>
2025-03-26 17:06:32 +0100

    figuring the nodejs out

--- plump/src/app.module.ts
+++ plump/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { \[user\]Controller } from './[user/]/[user/].controller';
+import { UserController } from './user/user.controller';
+
+//we need a couple more imports for sqlite such as typeorm and sqlite 3
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, \[user\]Controller, UserController],
   providers: [AppService],
 })
 export class AppModule {}
