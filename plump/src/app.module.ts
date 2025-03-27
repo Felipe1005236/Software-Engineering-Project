6e936bdc01b5e64bff4dae0bfa1660cf4021c11a
FelipeRibadeneira <fribadenei@constructor.university>
2025-03-27 20:53:21 +0100

    trying to make the prisma work

--- plump/src/app.module.ts
+++ plump/src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { \[user\]Controller } from './[user/]/[user/].controller';
-import { UserController } from './user/user.controller';
+import { UsersModule } from './users/users.module';
 
 //we need a couple more imports for sqlite such as typeorm and sqlite 3
 
 @Module({
-  imports: [],
-  controllers: [AppController, \[user\]Controller, UserController],
+  imports: [UsersModule],
   providers: [AppService],
 })
 export class AppModule {}
