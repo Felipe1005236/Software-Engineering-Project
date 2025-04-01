b85c47e08df23abf72fbfa2385fc35c5f2a076d7
FelipeRibadeneira <fribadenei@constructor.university>
2025-04-01 16:33:03 +0200

    Changes to the app module

--- plump/src/app.module.ts
+++ plump/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ProjectsModule } from './plump/projects/projects.module';
 
 @Module({
-  imports: [ProjectsModule],
 })
 export class AppModule {}
 
