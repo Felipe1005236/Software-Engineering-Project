151f1fc669638c411a307f833a3baed6856b054c
FelipeRibadeneira <fribadenei@constructor.university>
2025-04-27 20:11:00 +0200

    changes to the pahts to make it make sense

--- plump/src/app.module.ts
+++ plump/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BudgetModule } from './budget/budget.module';
 import { PrismaService } from './prisma/prisma.service';
+import { ProjectsModule } from './projects/projects.module';  
 
 @Module({
-  
-  imports: [BudgetModule],
-
+  imports: [BudgetModule, ProjectsModule],
   providers: [PrismaService]
 })
 export class AppModule {}
-
--- plump/src/projects/projects.module.ts
+++ plump/src/projects/projects.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
-import { PrismaService } from '../../prisma/prisma.service'; // Import PrismaService
+import { PrismaService } from '../prisma/prisma.service'; // Import PrismaService
 
 @Module({
   controllers: [ProjectsController], // Add the controller
