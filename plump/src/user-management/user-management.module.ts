b5197aa029ba761cb2356e2a16d5b34faf9b702c
Bilal Waraich <157110822+Bilal-Waraich@users.noreply.github.com>
2025-04-27 23:06:39 +0200

    User mangement fixes 1.0

--- plump/src/user-management/user-management.module.ts
+++ plump/src/user-management/user-management.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserManagementController } from './user-management.controller';
 import { UserManagementService } from './user-management.service';
+import { PrismaService } from 'src/prisma/prisma.service'; //Importing Prisma Service
 
 @Module({
   controllers: [UserManagementController],
-  providers: [UserManagementService],
+  providers: [UserManagementService, PrismaService],
 })
 export class UserManagementModule {}
