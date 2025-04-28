4387eb3f145695d8fad2f27681cde3a16da79cc0
FelipeRibadeneira <fribadenei@constructor.university>
2025-04-28 20:45:15 +0200

    Connected backend and frontend for projects

--- plump/src/main.ts
+++ plump/src/main.ts
@@ -4,6 +4,14 @@ import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:8080', // allow frontend container to access backend
+  });
+  
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
+
+
 }
 bootstrap();
